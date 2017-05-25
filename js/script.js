3bbc44fa1901a5d2b1e7fa492e0949a278a4c10a
mehta-aken <mehta.aken@gmail.com>
2017-05-25 12:43:10 -0400

    got data from api to display on the screen

--- js/script.js
+++ js/script.js
@@ -1,25 +1,88 @@
 var festivals = {}
-
-
-//getting data from api
-festivals.getData = function(){
-	$.ajax({
-		url: 'http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR?limit=3',
-		method: 'GET',
-		data: {
-			limit: 3,
-			format: 'json'
-		}
-	}).then(function(res){
-		console.log(res);
-	});
+festivals.todayDate = (new Date()).toJSON().split('').slice(0, 10).join('');
+festivals.thumbImagesUrl = 'http://app.toronto.ca';
+festivals.features = {
+	'Paid Parking' : '',
+	'Onsite Food and Beverages': '',
+	'Free Parking': '',
+	'Public Washrooms': '',
+	'Bike Racks': ''
 }
+
+$(function(){
+	festivals.init();
+});
 
 // initializing the functions on load
 festivals.init = function(){
 	festivals.getData();
 }
 
-$(function(){
-	festivals.init();
-});
+//getting data from api
+festivals.getData = function(){
+	$.ajax({
+		url: 'http://app.toronto.ca/cc_sr_v1_app/data/edc_eventcal_APR?',
+		method: 'GET',
+		data: {
+			limit: 403
+		}
+	}).then(function(res){
+		festivals.getItemsToDisplayData(res);
+	});
+}
+
+//get items from the ajax request
+festivals.getItemsToDisplayData = function(res){
+	var counter = 0;
+	res.forEach(function(e){
+		var event = e.calEvent;
+		var datesArray = event.dates;
+		datesArray.forEach(function(date){
+			if( date.hasOwnProperty('startDateTime') ){
+				var eventDate = (date.startDateTime).split('')
+													.slice(0, 10)
+													.join('');
+				if(festivals.todayDate === eventDate){
+					var eventName = event.eventName;
+					var isFree = event.freeEvent;
+					var cost = event.otherCostInfo;
+					var longEventDescription = event.description;
+					var shortEventDescription = longEventDescription.split(' ')
+																	.slice(0, 15)
+																	.join(' ') + '...';
+					var accessibility = event.accessibility;
+					var features = event.features;
+					var featuresArray = [];
+					var categories = event.categoryString;
+					if(event.image !== undefined){
+						var thumbImage = event.image.url;
+					}
+					counter++;
+					console.log(counter);
+					console.log(eventName);
+					// console.log('Cost: ' + event.otherCostInfo);
+					for(var feature in features){
+						if(features[feature])
+							featuresArray.push(feature);
+					}
+					console.log(categories);
+					console.log(featuresArray);
+					console.log(thumbImage);
+					festivals.displayToIndex(thumbImage, eventName, isFree, cost, shortEventDescription, featuresArray, accessibility, categories)
+				} 
+			}
+		});
+	});
+}
+
+festivals.displayToIndex = function(thumbImage, eventName, isFree, cost, shortEventDescription, featuresArray, accessibility, categories){
+	var image = $('<img>').attr({
+		src: `${festivals.thumbImagesUrl + thumbImage}`,
+		alt: 'Event thumbnail'
+	}).css('width', '200px');
+	var titleElement = $('<h2>').html(eventName);
+	var dateElement = $('<p>').html(festivals.todayDate);
+	var isFreeElement = $('<p>').html(`Free: ${isFree}`);
+	var shortEventDescriptionElement = $('<p>').html(shortEventDescription);
+	$('main').append(image, titleElement, dateElement, isFreeElement, shortEventDescriptionElement);
+}
